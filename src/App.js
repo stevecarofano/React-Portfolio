0e77b1a2756f52881a155aaf315331b1ccff796f
Steve C <stevencarofano@gmail.com>
2021-09-08 16:14:42 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header";
-import About from "./components/pages/About"
-import Home from "./components/pages/home"
+import About from "./components/pages/About";
+import Home from "./components/pages/home";
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/About' exact component={About}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/About" exact component={About} />
         </Switch>
       </Router>
     </>
