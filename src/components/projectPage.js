7b3774cb1bf9c5c12980f7fa7d5a986b716045e6
Steve C <stevencarofano@gmail.com>
2021-09-08 22:49:01 -0400

    Finalizing projects page

--- src/components/projectPage.js
+++ src/components/projectPage.js
@@ -1,14 +1,43 @@
 import React from "react";
 import "../App.css";
 import anifind from "../images/anifind.png";
+import weathertracker from "../images/weathertracker.png";
+import scheduler from "../images/scheduler.png";
+import tracker from "../images/tracker.png";
+import notetaker from "../images/notetaker.png";
+import budget from "../images/budget.png";
 
 function ProjectsPage() {
   return (
-    <div className="projectContainer">
-      <div className="aniFind">
-        <p>AniFind</p>
+    <div class="projectContainer">
+      <div class="aniFind">
         <a href="https://mbalasuriyar.github.io/Find-an-Anime-For-You/">
           <img src={anifind} alt=""></img>
+        </a>
+      </div>
+      <div class="tracker">
+        <a href="https://stevecarofano.github.io/Weather-Tracker/">
+          <img src={weathertracker} alt=""></img>
+        </a>
+      </div>
+      <div class="scheduler">
+        <a href="https://stevecarofano.github.io/Work-Day-Planner/">
+          <img src={scheduler} alt=""></img>
+        </a>
+      </div>
+      <div class="noteTaker">
+        <a href="https://github.com/stevecarofano/Note-taker">
+          <img src={notetaker} alt=""></img>
+        </a>
+      </div>
+      <div class="empTracker">
+        <a href="https://github.com/stevecarofano/Employee-Tracker">
+          <img src={tracker} alt=""></img>
+        </a>
+      </div>
+      <div class="budget">
+        <a href="https://github.com/stevecarofanoAdaptable-Budget-Tracker">   
+          <img src={budget} alt=""></img>
         </a>
       </div>
     </div>
